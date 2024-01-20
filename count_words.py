43ccf9f765bf46a0a65cd982b5188deb62a9392f
Cristian Ciacu <d.c.ciacu@student.tudelft.nl>
2024-01-20 23:17:23 +0100

    Add program

---
+++ count_words.py
@@ -0,0 +1,35 @@
+import sys
+import argparse
+
+def main():
+    parser = argparse.ArgumentParser(description='Find the number of words from a Latex text.')
+    parser.add_argument('-str', help='Input string', nargs='?', type=str, const=None)
+
+    args = parser.parse_args()
+
+    if not args.str:
+        print("Ask for help. -h or --h")
+        return
+    # if len(sys.argv) != 2:
+    #     print("Too many or too few params.")
+    #     return
+    input = sys.argv[1]
+    index = input.find("\citet")
+    while index != -1:
+        input = input[:index - 1] + input[index + 6 :]
+        index = input.find("citet")
+
+    start_index = input.find("{")
+    end_index = input.find("}")
+
+    while start_index != -1 and end_index != -1:
+        input = input[:start_index - 1] + input[end_index + 1:]
+        start_index = input.find("{")
+        end_index = input.find("}")
+
+    words = input.split(" ")
+    print(f"\nInput string has: {len(words)} words.\n") 
+
+
+if __name__ == '__main__':
+    main();
